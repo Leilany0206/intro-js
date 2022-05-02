552e67b30ad8e5ccb9a22097ae6688f04043d7cd
Leilany0206 <101684710+Leilany0206@users.noreply.github.com>
2022-05-01 23:52:15 -0500

    Add files via upload

---
+++ Actividad 3/1.js
@@ -0,0 +1,7 @@
+var dividendo;
+dividendo = prompt ("Introduce un número random");
+if ( ( dividendo % 2 ) == 0 ) {
+    "x número es divisible entre 2"
+} else {
+    "x número no es divisible entre 2"
+}
